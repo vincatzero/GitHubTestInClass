71429d90f368d514f1bfe78cff65a368a739bd1f
Omar <ocataldo@carthage.edu>
2020-02-25 11:05:36 -0600

    Fixed parameters and return type conflict
    Power() takes in a double and returns a double

--- main.cpp
+++ main.cpp
@@ -2,27 +2,35 @@
 
 using namespace std;
 
-void power(int base, int exp);
+double power(double base, int exp);
 
 int main()
 {
-    cout<<"GitHub Test"<<endl;
-    
-    power(10, 2);
-    power(5.3, 3);
-    power(7.7, 1);
-    power(3.9, 0);
+    cout << "GitHub Test" << endl;
+
+   double y = power(10, 2);
+   cout << y <<endl;
+    y = power(5.3, 3);
+    cout << y << endl;
+
+    y = power(7.7, 1);
+    cout << y << endl;
+
+    y = power(3.9, 0);
+    cout << y << endl;
+
 
     return 0;
 }
 //--
-void power(double base, int exp)
+
+double power(double base, int exp)
 {
     double result = 1;
-    for(int i = 0;i < exp;i++)
+    for (int i = 0; i < exp; i++)
     {
         result = result * base;
     }
 
-    cout<<result<<endl;
+    return result;
 }
