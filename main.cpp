24ec0fbc0d1a4965d575712a8e8d7bf99b265d6b
Mark Mahoney <mmahoney@carthage.edu>
2020-02-25 10:34:38 -0600

    Added  a square() function.
    This function takes an int and prints the squared value.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
+void square(int num);
+
 int main()
 {
     cout<<"GitHub Test"<<endl;
-
+    square(10);
     return 0;
+}
+//--
+void square(int num)
+{
+    int result = num * num;
+    cout<<result<<endl;
 }
